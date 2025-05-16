130cc1d3dac310ca14ac413f66f078e0a7b7594e
Sparkwave <Plazmoid3@gmail.com>
2025-05-16 11:56:40 +0200

    Add project template

---
+++ common/src/main/java/dev/titanite/sparkwave/sword_saint/ExampleMod.java
@@ -0,0 +1,9 @@
+package dev.titanite.sparkwave.sword_saint;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "sword_saint";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
